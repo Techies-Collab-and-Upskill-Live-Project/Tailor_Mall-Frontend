c3054fdf286cb8a17bd59ad81dd2f770582d5f11
Theecypher <obioramma12@gmail.com>
2024-08-27 11:41:29 +0100

    Linked the welcome page to the sign up page

--- src/components/Button/BigButton.jsx
+++ src/components/Button/BigButton.jsx
@@ -1,13 +1,11 @@
 import { Link } from "react-router-dom";
 
-const BigButton = ({text, handleSubmit}) => {
+const BigButton = ({text, handleSubmit, className, to }) => {
   return (
     <div>
       <Link
-        // className={`px-4 py-2 rounded-3xl text-[6px] md:text-base ${
-        //   isMain ? "bg-blue-900 text-white" : "border border-black text-black"
-        // }`}
-        className="rounded-[100px] flex items-center justify-center px-[12px] bg-[#008080] h-10 text-[#E6F2F2] text-[12px] leading-4 font-medium md:text-base md:leading-[16.8px] lg:text-[16px] md:w-full"
+      to={to}
+        className={`rounded-[100px] flex items-center justify-center px-[12px] bg-[#008080] h-10 text-[#E6F2F2] text-[12px] leading-4 font-medium md:text-base md:leading-[16.8px] lg:text-[16px] md:w-full ${className}`}
       >
         <span onSubmit={handleSubmit} className="text-center"> {text} </span>
       </Link>
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,8 +2,15 @@ import { Link } from "react-router-dom";
 import BigButton from "../Button/BigButton";
 import ButtonLink from "../Button/Button";
 import { WelcomeData } from "./WelcomeData";
+import { useState } from "react";
 
 const Welcome = () => {
+  const [active, setActive] = useState(null);
+
+  const onActive = (index) => {
+    setActive(index)
+  };
+
   return (
     <div className="flex flex-col absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]">
       <div className="flex flex-col gap-3 md:gap-8">
@@ -16,8 +23,12 @@ const Welcome = () => {
         <div className="flex flex-col md:flex-row gap-3">
           {WelcomeData.map((item, index) => (
             <Link
+              onClick={() => onActive(index)}
               key={index}
-              className="flex flex-col gap-[6px] border p-5 w-[300px] md:w-fit border-[#BCBCBC] rounded-3xl"
+              className={`flex flex-col gap-[6px] border p-5 w-[300px] md:w-fit rounded-3xl transition duration-300 ${
+                active === index ? 'border-blue-500 bg-blue-400' : 'border-[#BCBCBC]'
+              }`}
+              // className={`flex flex-col gap-[6px] border p-5 w-[300px] md:w-fit border-[#BCBCBC] rounded-3xl  activeIndex === index ? 'border-blue-500 bg-blue-100' : 'border-[#BCBCBC]'`}
             >
               <h3 className="text-base font-bold text-[#111] md:text-xl lg:text-2xl">
                 {item.title}
@@ -30,7 +41,11 @@ const Welcome = () => {
         </div>
 
         <div className="flex flex-col justify-center items-center gap-4">
-          <BigButton text="Create an account" />
+          <BigButton
+            className="w-[300px] md:w-[450px]"
+            text="Create an account"
+            to="/signup"
+          />
 
           <p className="font-normal text-sm leading-4 text-center md:text-base md:leading-[22.4px] lg:text-xl">
             I’m already a user
